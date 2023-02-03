0e25354e17ae0c3d57005d415f857960282675a3
Bang NGUYEN <le-ly-bang.nguyen@etu.utc.fr>
2023-02-03 10:18:03 +0100

    All level files are correctly created

--- src/levels.sh
+++ src/levels.sh
@@ -5,6 +5,7 @@ load_level() {
 	local dest_objects_folder="../current_lelvel/objects"
 
 	rm -rf $dest_map_folder
+	rm -rf $dest_objects_folder
 	
 	# Create folders
 	mkdir -p $dest_map_folder
@@ -17,15 +18,11 @@ load_level() {
 	let "n_cols=0"
 	while read line; do
 		let "n_rows=n_rows+1"
-		let "n_cols=0"
-		echo $line
-		for cell in $line
-		do
-			let "n_cols=n_cols+1"
-			echo $n_rows
-			echo $n_cols
-			echo $cell
-			echo $cell > "$dest_map_folder/${n_rows}_${n_cols}"
+		let "col=0"
+
+		echo $line | fold -w 1 | while read c; do
+			let "col=col+1"
+			echo $cell > "$dest_map_folder/${n_rows}_${col}"
 		done
 	done < $src_file
 }
