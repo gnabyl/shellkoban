211ca4c964cc46ec7aa2bc2b91a83f24d5a86eaa
shao <shao_xinghua@outlook.com>
2023-02-03 09:57:06 +0100

    ajouter redemarrerLevel

--- src/main.sh
+++ src/main.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
 # This the main code file
+
+# fonction de Rédemarrer level
+# Taper <R> pour Rédemarrer level
+M = 10
+N = 10
+redemarrerLevel() {
+	for  ((ligne=0; ligne<$M; ligne++))
+	do
+		for ((colonne=0; colonne<$N; colonne++))
+		do
+			
+}
