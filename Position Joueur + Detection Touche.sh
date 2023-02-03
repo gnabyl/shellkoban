c54aee04941a489bf3e766e8e26c16e61d6e832b
Ahmad Misselmani <ahmad.misselmani@etu.utc.fr>
2023-02-03 09:56:12 +0000

    Update Position Joueur + Detection Touche.sh

--- Position Joueur + Detection Touche.sh
+++ Position Joueur + Detection Touche.sh
@@ -4,22 +4,22 @@ do
     read -rsn 1 t
     case $t in
         A) validation $((gr-1)) $gc $((gr-2)) $gc; 
-            if [ "$va" == "1"];
+            if [ "$va" == "1" ]
             then
                 gr=$((gr-1))
             fi;;
         B) validation $(($gr+1)) $gc $((gr-2)) $gc;
-            if [ "$va" == "1"];
+            if [ "$va" == "1" ]
             then
                 gr=$((gr+1))
             fi ;;
         C) validation $gr $((gc+1)) $gr $((gc+2))
-            if [ "$va" == "1"];
+            if [ "$va" == "1" ]
             then
                 gr=$((gc+1))
             fi ;;
         D) validation $gr $((gc-1)) $gr $((gc-2))
-            if [ "$va" == "1"];
+            if [ "$va" == "1" ]
             then
                 gr=$((gc-1))
             fi;;
