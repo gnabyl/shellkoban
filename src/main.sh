674feebd850f540b90930db0bb69190138bdc30a
Bang NGUYEN <le-ly-bang.nguyen@etu.utc.fr>
2023-02-03 10:52:13 +0100

    Main game loop

--- src/main.sh
+++ src/main.sh
@@ -1,1 +1,23 @@
-# This the main code file
+source "./levels.sh"
+
+let "current_level=1"
+let "max_level=1"
+
+while [ $current_level -le $max_level ]
+do
+	# Load the current level
+	load_level $current_level
+
+	# The user didn't win
+	win=false
+
+	# Main game loop
+	while [ $win = false ]
+	do
+		echo "Run"
+		# Wait for user input
+		# Check validity
+		# Calculate new position
+		# Display map
+	done
+done
