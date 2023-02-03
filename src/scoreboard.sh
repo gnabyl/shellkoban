192ba7aa153515007f23b2d71092397d97f6b31a
shao <shao_xinghua@outlook.com>
2023-02-03 10:53:47 +0100

    add gestion et affichage de scoreboard

---
+++ src/scoreboard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# créer 5 dossier pour chaque niveau dans dossier score
+# fichier scoreboard.txt
+# fonction 1: écrire scoreboard dans fichier scoreboard.txt
+#   il faut écrire nom_joueur:score
+
+current_level
+write_score() {
+	# D'abord on prend le curret_level
+	# Ensuite on prend le nom_joueur
+	# Puis on prend 
+}
