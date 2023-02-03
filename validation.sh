2abf5ef1e29e8995e621bdd99d12bd7c03a624f4
Reda <reda.taleb.tal@gmail.com>
2023-02-03 11:07:51 +0100

    validation corrigée 2

--- validation.sh
+++ validation.sh
@@ -10,11 +10,11 @@ local_colplus=$4
 if [ "#" -eq $(cat ${local_row}_${local_col}) ]
 	then 
 	$va=0
-elif [ @ -eq $(cat ${local_row}_${local_col}) && "#" -eq (cat ${local_rowplus}_${local_colplus}) ]
+elif [ "@" -eq $(cat ${local_row}_${local_col}) && "#" -eq (cat ${local_rowplus}_${local_colplus}) ]
 	then 
 	$va=0
 
-elif [ @ -eq $(cat ${local_row}_${local_col}) && @ -eq (cat ${local_rowplus}_${local_colplus}) ]
+elif [ "@" -eq $(cat ${local_row}_${local_col}) && "@" -eq (cat ${local_rowplus}_${local_colplus}) ]
 	then	
 	$va=0
 else 
