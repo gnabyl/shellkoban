e37cfcf007adb2b5b993888c9d976da63f7d689e
Bang NGUYEN <le-ly-bang.nguyen@etu.utc.fr>
2023-02-03 16:10:14 +0100

    WORKED

--- src/clavier.sh
+++ src/clavier.sh
@@ -1,5 +1,6 @@
 source "./validation.sh"
 source "./modif_gamer.sh"
+source "./pos_box_update.sh"
 
 ecoute_touche () {
     read -rsn 1 t
@@ -8,24 +9,28 @@ ecoute_touche () {
             if [ "$va" == "1" ]
             then
 							modif_gamer $gr $gc $((gr-1)) $gc
+							modif_box $((gr-2)) $gc
               gr=$((gr-1))
             fi;;
-        B) validation $(($gr+1)) $gc $((gr-2)) $gc;
+        B) validation $(($gr+1)) $gc $((gr+2)) $gc;
             if [ "$va" == "1" ]
             then
 							modif_gamer $gr $gc $((gr+1)) $gc
-							gr=$((gr+1))
+							modif_box $((gr+2)) $gc
+							gr=$((gr+1)) 
             fi ;;
         C) validation $gr $((gc+1)) $gr $((gc+2))
             if [ "$va" == "1" ]
             then
 							modif_gamer $gr $gc $gr $((gc+1)) 
+							modif_box $gr $((gc+2))
 							gc=$((gc+1))
             fi ;;
         D) validation $gr $((gc-1)) $gr $((gc-2))
             if [ "$va" == "1" ]
             then
 							modif_gamer $gr $gc $gr $((gc-1)) 
+							modif_box $gr $((gc-2))
 							gc=$((gc-1))
             fi;;
     esac
--- src/pos_box_update.sh
+++ src/pos_box_update.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #prend en parametre les coordonnes de la position apres ou le joueur va atterir
-modifBox () {
+modif_box () {
 	if [ "$vb" == "1" ]
 	then
-		touch "../current_level/objects/${1}_${2}"
 		echo "@" > "../current_level/objects/${1}_${2}"
 	fi 
 }
