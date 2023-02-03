6d752c267a03ac83aeb5367e906be1e397e2dbf5
Ahmad Misselmani <ahmad.misselmani@etu.utc.fr>
2023-02-03 09:39:15 +0000

    Version 2

--- script mettre a jour position avec touche/script.sh
+++ script mettre a jour position avec touche/script.sh
@@ -3,17 +3,25 @@ while true
 do
     read -rsn 1 t
     case $t in
-        A) #verification validite
-	#si valide: 
-		gr=$(($gr+1)) ;;
-        B) #verification validite
-        #si valide: 
-		gr=$(($gr-1)) ;;
-        C) #verification validite
-        #si valide: 
-		gc=$(($gc+1)) ;;
-        D) #verification validite
-        #si valide: 
-		gc=$(($gc-1)) ;;
+        A) validation $((gr-1)) $gc $((gr-2)) $gc; 
+            if [ "$va" == "1"];
+            then
+                gr=$((gr-1)) ;;
+            fi
+        B) validation $(($gr+1)) $gc $((gr-2)) $gc;
+            if [ "$va" == "1"];
+            then
+                gr=$((gr+1)) ;;
+            fi ;;
+        C) validation $gr $((gc+1)) $gr $((gc+2))
+            if [ "$va" == "1"];
+            then
+                gr=$((gc+1))
+            fi ;;
+        D) validation $gr $((gc-1)) $gr $((gc-2))
+            if [ "$va" == "1"];
+            then
+                gr=$((gc-1))
+            fi;;
     esac
 done
